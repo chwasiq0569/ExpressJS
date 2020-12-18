e045093a2993ec91bf3db56a80f26d3efe79d10e
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-18 03:32:59 -0800

    Nested Callbacks

--- asyncJS/index.js
+++ asyncJS/index.js
@@ -1,12 +1,24 @@
 console.log("Before");
 // const data = getUser(1); //here data will be undefined because the value returned by getUser function after 2 sec is assigned to data and when data is logged it will show undefined because at that is synchronous operation and at that time noting in assigned to data
 // console.log(data);
-getUser(1, (user) => console.log("User: ", user));
+getUser(1, (user) => {
+  console.log("User: ", user);
+  getRepositories(user.githubUsername, (repos) => {
+    console.log("repos: ", repos);
+  });
+});
 console.log("After");
-
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("Getting Data from database...");
     callback({ id: id, githubUsername: "chwasiq0569" });
+  }, 2000);
+}
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Getting Repos from database...");
+    if (username === "chwasiq0569") {
+      callback(["repo1", "repo2"]);
+    }
   }, 2000);
 }
