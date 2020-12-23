142c50ba6cf2e928b516adf102e4999e13e72a61
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-22 21:49:42 -0800

    Authentication

---
+++ Authentication/hash.js
@@ -0,0 +1,8 @@
+const bcrypt = require("bcrypt");
+
+const run = async () => {
+  console.log("salt: ", salt);
+  console.log("hashed: ", hashed);
+};
+
+run();
--- Authentication/register.js
+++ Authentication/register.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json()); //middleware
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 mongoose
   .connect("mongodb://localhost/auth")
@@ -46,12 +47,29 @@ app.post("/register", (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) res.status(400).send("User already exists...");
     user = new User(_.pick(req.body, ["username", "email", "password"]));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     try {
-      const result = await user.save();
+      await user.save();
       res.send(_.pick(req.body, ["username", "email"]));
     } catch (err) {
       console.log("Error Occured in Creating New User: ", err.message);
     }
+  };
+  createUser();
+});
+
+app.post("/login", (req, res) => {
+  const createUser = async () => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) res.status(400).send("Invalid email or password...");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) res.status(400).send("Invalid email or password...");
+    console.log(validPassword);
+    res.send(true);
   };
   createUser();
 });
